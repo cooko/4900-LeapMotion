779843615015e88988872bf2025a6bb1d01b098f
cooko <cooko@uoguelph.ca>
2013-11-07 14:54:48 -0500

    Adding Leap Motion Development Files

---
+++ compile-debug.bat
@@ -0,0 +1,7 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
+del .\build\debug\Leapdev.exe
+cl /EHsc /Zi /I .\include .\source\Leapdev.cpp /link .\lib\debug\Leapd.lib /OUT:.\build\debug\Leapdev.exe /DEBUG
+del Leapdev.obj
+del vc120.pdb
+del .\build\debug\Leapdev.ilk
+del .\build\debug\Leapdev.pdb
---
+++ source/Leapdev.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************\
+* Copyright (C) 2012-2013 Leap Motion, Inc. All rights reserved.               *
+* Leap Motion proprietary and confidential. Not for distribution.              *
+* Use subject to the terms of the Leap Motion SDK Agreement available at       *
+* https://developer.leapmotion.com/sdk_agreement, or another agreement         *
+* between Leap Motion and you, your company or other organization.             *
+\******************************************************************************/
+
+#include <iostream>
+#include "Leap.h"
+using namespace Leap;
+
+int main() {
+	Controller controller;
+
+	std::cout << "Press Enter to quit..." << std::endl;
+	std::cin.get();
+
+	return 0;
+}
