74d8258d219068d5869069fac48f0a8f4b363b41
cooko <cooko@uoguelph.ca>
2013-11-10 23:49:42 -0500

    Added complete sample code

--- source/Leapdev.cpp
+++ source/Leapdev.cpp
@@ -10,11 +10,90 @@
 #include "Leap.h"
 using namespace Leap;
 
+int handCount = 0;
+int fingerCount = 0;
+
+int gold = 0;
+int track = 0;
+int trackCount = 0;
+
+class SampleListener : public Listener {
+public:
+	virtual void onInit(const Controller&);
+	virtual void onConnect(const Controller&);
+	virtual void onDisconnect(const Controller&);
+	virtual void onExit(const Controller&);
+	virtual void onFrame(const Controller&);
+	virtual void onFocusGained(const Controller&);
+	virtual void onFocusLost(const Controller&);
+};
+
+void SampleListener::onInit(const Controller& controller) {
+	std::cout << "Initialized" << std::endl;
+}
+
+void SampleListener::onConnect(const Controller& controller) {
+	std::cout << "Connected" << std::endl;
+	controller.enableGesture(Gesture::TYPE_CIRCLE);
+	controller.enableGesture(Gesture::TYPE_KEY_TAP);
+	controller.enableGesture(Gesture::TYPE_SCREEN_TAP);
+	controller.enableGesture(Gesture::TYPE_SWIPE);
+}
+
+void SampleListener::onDisconnect(const Controller& controller) {
+	//Note: not dispatched when running in a debugger.
+	std::cout << "Disconnected" << std::endl;
+}
+
+void SampleListener::onExit(const Controller& controller) {
+	std::cout << "Exited" << std::endl;
+}
+
+void SampleListener::onFrame(const Controller& controller) {
+	// Get the most recent frame and report some basic information
+	const Frame frame = controller.frame();
+	if (frame.hands().count() != handCount){
+		handCount = frame.hands().count();
+		std::cout << frame.hands().count() << " Hands\n";
+	}
+	if (frame.fingers().count() != fingerCount){
+		fingerCount = frame.fingers().count();
+	}
+	if (frame.fingers().count() != track){
+		track = frame.fingers().count();
+		trackCount = 1;
+	}
+	else{
+		trackCount++;
+		if (trackCount == 10){
+			gold = track;
+			std::cout << frame.fingers().count() << " Fingers\n";
+		}
+	}
+}
+
+void SampleListener::onFocusGained(const Controller& controller) {
+	std::cout << "Focus Gained" << std::endl;
+}
+
+void SampleListener::onFocusLost(const Controller& controller) {
+	std::cout << "Focus Lost" << std::endl;
+}
+
 int main() {
+	// Create a sample listener and controller
+	SampleListener listener;
 	Controller controller;
 
+	// Have the sample listener receive events from the controller
+	controller.addListener(listener);
+
+	// Keep this process running until Enter is pressed
 	std::cout << "Press Enter to quit..." << std::endl;
 	std::cin.get();
+
+	// Remove the sample listener when done
+	controller.removeListener(listener);
 
 	return 0;
 }
